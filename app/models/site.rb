13d2a6245d22720829e9430d1261d3944430f0d5
billperegoy <bill@billperegoy.com>
2015-01-24 22:23:53 -0500

    Added callback to Site model to create empty installations upon creation of new site.

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,10 @@
 class Site < ActiveRecord::Base
+  after_save :create_empty_installations, on: [ :create ]
+
+  protected
+  def create_empty_installations
+    Version.all.each do |version|
+      Installation.create(state: 'uninstalled' , version_id: version.id , site_id: self.id)
+    end
+  end
 end
