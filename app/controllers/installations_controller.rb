3cb64587d71da0945053090479e442b563c2515b
billperegoy <bill@billperegoy.com>
2015-03-14 20:49:35 -0400

    Added placeholders for actual install actions.

--- app/controllers/installations_controller.rb
+++ app/controllers/installations_controller.rb
@@ -1,2 +1,32 @@
 class InstallationsController < ApplicationController
+  def install_one
+    @tool = Tool.find(params[:tool_id])
+    @version = Version.find(params[:version_id])
+    @installation = Installation.find(params[:id])
+
+    # FIXME - This is where I do the actual install
+    #         Currently I render a view here but this
+    #         shpuld start the installa and redirect.
+
+    # redirect_to_version_page
+  end
+
+  def install_all
+    @tool = Tool.find(params[:tool_id])
+    @version = Version.find(params[:version_id])
+    @installations = @version.installations
+
+    # FIXME - This is where I do the actual install
+    #         Currently I render a view here but this
+    #         shpuld start the installa and redirect.
+    #
+    # redirect_to_version_page
+  end
+
+  private
+  def redirect_to_version_page
+    redirect_to tool_version_path(
+      tool_id: @tool_id,
+      id: @version_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
       resources :installations
     end
   end
+
+  post '/tools/:tool_id/versions/:version_id/installations/:id',
+    to: 'installations#install_one',
+    as: 'tool_install_one'
+
+  post '/tools/:tool_id/versions/:version_id',
+    to: 'installations#install_all',
+    as: 'tool_install_all'
 end
